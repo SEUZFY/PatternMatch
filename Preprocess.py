ed02687a91706ea47b4e46e07b7f5ee7e99eea3e
zmocheng <zmocheng@gmail.com>
2021-12-11 21:51:06 +0100

    Use one column for x and y.

--- Preprocess.py
+++ Preprocess.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 # open file
-file_object = open(r"D:\AlbertQ2\GEO1003\PatternMatch\Geolab.txt", encoding='utf-8')
+file_object = open(r"D:\AlbertQ2\GEO1003\PatternMatch\loc4.txt", encoding='utf-8')
 try:
     file_content = file_object.readlines() # not very huge file, use readlines() to speed up
 finally:
@@ -15,5 +15,5 @@ for each in file_content:
     mac.append(each.split(splitor)[1]) # mac address
     signal.append(float(each.split(splitor)[4])) # signal strength
 
-pd.DataFrame(signal).to_csv(r"D:\AlbertQ2\GEO1003\PatternMatch\signal.csv")
-pd.DataFrame(mac).to_csv(r"D:\AlbertQ2\GEO1003\PatternMatch\mac.csv")
+pd.DataFrame(signal).to_csv(r"D:\AlbertQ2\GEO1003\PatternMatch\loc4signal.csv")
+pd.DataFrame(mac).to_csv(r"D:\AlbertQ2\GEO1003\PatternMatch\loc4mac.csv")
